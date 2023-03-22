e40cb4779d52baa0060fcf108f48101541daef21
Paul Boschert <paul@globalstreaming.com>
2023-03-22 20:04:56 +0100

    adding C# training module code

---
+++ TestProject/Program.cs
@@ -0,0 +1,10 @@
+﻿
+Random examScores = new Random();
+
+int students[][] = {
+    random.Next(0, 101),
+    random.Next(0, 101),
+    random.Next(0, 101),
+    random.Next(0, 101),
+    random.Next(0, 101)
+}
