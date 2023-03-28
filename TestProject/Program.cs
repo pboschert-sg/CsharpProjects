a4bcbbb88f462ef749d9e85793f82c6376c56f52
Paul Boschert <paul@streamingglobal.com>
2023-03-28 21:20:27 +0100

    Adding code for module: Branch the flow of code using the switch-case construct in C#

--- TestProject/Program.cs
+++ TestProject/Program.cs
@@ -1,18 +1,54 @@
-﻿
-string permission = "Admin|Manager";
-int level = 55;
+﻿// SKU = Stock Keeping Unit. 
+// SKU value format: <product #>-<2-letter color code>-<size code>
+string sku = "01-MN-L";
 
+string[] product = sku.Split('-');
 
+string type = "";
+string color = "";
+string size = "";
 
-if (permission.Contains("Admin")) {
-    if (level > 55)
-        Console.WriteLine("Welcome, Super Admin user.");
-    else
-        Console.WriteLine("Welcome, Admin user.");
-} else if (permission.Contains("Manager")) {
-    if (level >= 20)
-        Console.WriteLine("Contact an Admin for access.");
-    else
-        Console.WriteLine("You do not have sufficient privileges.");
-} else
-    Console.WriteLine("You do not have sufficient privileges.");
+switch(product[0]) {
+    case "01":
+        type = "Sweat shirt";
+        break;
+    case "02":
+        type = "T-Shirt";
+        break;
+    case "03":
+        type = "Sweat pants";
+        break;
+    default:
+        type = "Other";
+        break;
+}
+
+switch (product[1]) {
+    case "BL":
+        color = "Black";
+        break;
+    case "MN":
+        color = "Maroon";
+        break;
+    default:
+        color = "White";
+        break;
+
+}
+
+switch(product[2]) {
+    case "S":
+        size = "Small";
+        break;
+    case "M":
+        size = "Medium";
+        break;
+    case "L":
+        size = "Large";
+        break;
+    default:
+        size = "One Size Fits All";
+        break;
+}
+
+Console.WriteLine($"Product: {size} {color} {type}");
