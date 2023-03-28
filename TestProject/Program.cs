4ddb48f9c83a205ebb3093f300a2e2a36ac5ad36
Paul Boschert <paul@streamingglobal.com>
2023-03-28 21:37:18 +0100

    Adding code for module: Iterate through a code block using for statement in C#

--- TestProject/Program.cs
+++ TestProject/Program.cs
@@ -1,54 +1,15 @@
-﻿// SKU = Stock Keeping Unit. 
-// SKU value format: <product #>-<2-letter color code>-<size code>
-string sku = "01-MN-L";
+﻿using System.Xml;
 
-string[] product = sku.Split('-');
-
-string type = "";
-string color = "";
-string size = "";
-
-switch(product[0]) {
-    case "01":
-        type = "Sweat shirt";
-        break;
-    case "02":
-        type = "T-Shirt";
-        break;
-    case "03":
-        type = "Sweat pants";
-        break;
-    default:
-        type = "Other";
-        break;
+string output = "";
+for (int i = 1; i <= 100; ++i) {
+    output = i.ToString();
+    if (i % 15 == 0)
+        output += " FizzBuzz";
+    else {
+        if (i % 3 == 0)
+            output += " Fizz";
+        else if (i % 5 == 0)
+            output += " Buzz";
+    }
+    Console.WriteLine(output);
 }
-
-switch (product[1]) {
-    case "BL":
-        color = "Black";
-        break;
-    case "MN":
-        color = "Maroon";
-        break;
-    default:
-        color = "White";
-        break;
-
-}
-
-switch(product[2]) {
-    case "S":
-        size = "Small";
-        break;
-    case "M":
-        size = "Medium";
-        break;
-    case "L":
-        size = "Large";
-        break;
-    default:
-        size = "One Size Fits All";
-        break;
-}
-
-Console.WriteLine($"Product: {size} {color} {type}");
